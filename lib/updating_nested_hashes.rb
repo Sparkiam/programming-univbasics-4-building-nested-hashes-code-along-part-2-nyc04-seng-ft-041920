2b929676d159de2b49675544919670133077fd90
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-04-28 16:09:44 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -1,27 +1,31 @@
 def update_status
-   epic_tragedy = {
-      :montague => {
-         :patriarch => {name: "Lord Montague", age: "53"},
-         :matriarch => {name: "Lady Montague", age: "54"},
+   epic_tragedy = {:montague => 
+        {:patriarch => {name: "Lord Montague", 
+              age: "53"},
+         :matriarch => {name: "Lady Montague", 
+              age: "54"},
          :hero => {name: "Romeo", age: "15", status: "alive"},
-         :hero_friends => [
-            {name: "Benvolio", age: "17", attitude: "worried"},
-            {name: "Mercutio", age: "18", attitude: "hot-headed"}
-         ]
-      },
+            :hero_friends => 
+                  [{name: "Benvolio", age: "17", attitude: "worried"},
+                  {name: "Mercutio", age: "18", attitude: "hot-headed"}]
+                   },
       :capulet => {
-         :patriarch => {name: "Lord Capulet", age: "50"},
-         :matriarch => {name: "Lady Capulet", age: "51"},
+         :patriarch => {name: "Lord Capulet", 
+              age: "50"},
+         :matriarch => {name: "Lady Capulet", 
+              age: "51"},
          :heroine => {name: "Juliet", age: "15", status: "alive"},
-         :heroine_friends => [
-         {name: "Steven", age: "30", attitude: "confused"},
-         {name: "Nurse", age: "44", attitude: "worried"}
-         ]
+         :heroine_friends =>
+                  [{name: "Steven", age: "30", attitude: "confused"},
+                  {name: "Nurse", age: "44", attitude: "worried"}
+                  ]
+               }
       }
-   }
+ 
 
-  # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
-  # Code your solution here:
+epic_tragedy[:montague][:hero][:status] = "dead"
+
+epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
 
@@ -31,27 +35,43 @@ def update_status
 end
 
 def add_characters
-   epic_tragedy = {
-      :montague => {
-         :patriarch => {name: "Lord Montague", age: "53"},
-         :matriarch => {name: "Lady Montague", age: "54"},
+  epic_tragedy = {:montague => 
+        {:patriarch => {name: "Lord Montague", 
+              age: "53"},
+         :matriarch => {name: "Lady Montague", 
+              age: "54"},
          :hero => {name: "Romeo", age: "15", status: "alive"},
-         :hero_friends => [
-            {name: "Benvolio", age: "17", attitude: "worried"},
-            {name: "Mercutio", age: "18", attitude: "hot-headed"}
-         ]
-      },
+            :hero_friends => 
+                  [{name: "Benvolio", age: "17", attitude: "worried"},
+                  {name: "Mercutio", age: "18", attitude: "hot-headed"}]
+                   },
       :capulet => {
-         :patriarch => {name: "Lord Capulet", age: "50"},
-         :matriarch => {name: "Lady Capulet", age: "51"},
+         :patriarch => {name: "Lord Capulet", 
+              age: "50"},
+         :matriarch => {name: "Lady Capulet", 
+              age: "51"},
          :heroine => {name: "Juliet", age: "15", status: "alive"},
-         :heroine_friends => [
-         {name: "Steven", age: "30", attitude: "confused"},
-         {name: "Nurse", age: "44", attitude: "worried"}
-         ]
-      }
-   }
+         :heroine_friends =>
+                  [{name: "Steven", age: "30", attitude: "confused"},
+                  {name: "Nurse", age: "44", attitude: "worried"}
+                  ]
+               },
+         :additional_characters =>
+                  [{ name: "Prince Escalus"},
+                  { name: "Apothecary"}]
+  },
+          
+ 
+ 
+ #epic_tragedy[additional_characters.length = 2]
+ #[:additional_characters][0][:name] = "Prince Escalus"
+ #[:additional_characters][1][:name] =
+  #  "Apothecary"
 
+ 
+ 
+ 
+ 
    # Write code below that does the following programmatically:
    #
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
@@ -61,10 +81,36 @@ def add_characters
    # Code your solution here:
 
 
+ epic_tragedy = {
+      :montague => {
+         :patriarch => {name: "Lord Montague", age: "53"},
+         :matriarch => {name: "Lady Montague", age: "54"},
+         :hero => {name: "Romeo", age: "15", status: "alive"},
+         :hero_friends => [
+            {name: "Benvolio", age: "17", attitude: "worried"},
+            {name: "Mercutio", age: "18", attitude: "hot-headed"}],
+      :capulet => {
+         :patriarch => {name: "Lord Capulet", age: "50"},
+         :matriarch => {name: "Lady Capulet", age: "51"},
+         :heroine => {name: "Juliet", age: "15", status: "alive"},
+         :heroine_friends => [
+         {name: "Steven", age: "30", attitude: "confused"},
+         {name: "Nurse", age: "44", attitude: "worried"}],
+      }
+    }
+}
 
-
-
-
+  epic_tragedy[0][:additional_characters] = []
+  prince = {:name => "Prince Escalus"}
+  apothecary = {:name => "Apothecary"}
+  
+ epic_tragedy[0][:additional_characters] << prince
+ epic_tragedy[0][:additional_characters] << apothecary
+  puts epic_tragedy[0][:additional_characters]
+  
+  
+      #epic_tragedy[:additional_characters][0][:name]["Prince Escalus"],
+      #epic_tragedy[:additional_characters][1][:name]["Apothecary"]
    # After your code, the following line will return the altered hash
-   epic_tragedy
-end
+ #epic_tragedy
+end 
